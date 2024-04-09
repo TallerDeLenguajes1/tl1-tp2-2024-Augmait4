abece264f61972350d67a9ae6a87cac70b384c61
Jose <augustomaitajam@gmail.com>
2024-04-09 13:59:44 -0300

    Agrego tp2_3.c

---
+++ tp2_3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+#include <time.h>
+#define N 5
+#define M 7
+
+int main(int argc, char const *argv[])
+{
+    int i,j;
+    int mt[N][M];
+    int *puntj;
+    puntj = mt;
+    srand(time(NULL));
+    for(i = 0;i<N; i++)
+        {
+            printf("| ");
+            for(j = 0;j<M; j++)
+                {
+                *puntj=1+rand()%100;
+                printf("%3i ", *puntj);
+                puntj ++;
+                }
+            printf("|");
+            printf("\n");
+        }    
+    return 0;
+}
